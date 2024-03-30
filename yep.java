1fdbd656488a715bc090b3dcc9ba078e953f57a8
Raihana-2964 <148611059+Raihana-2964@users.noreply.github.com>
2024-03-30 20:03:35 +0530

    Update yep.java

--- yep.java
+++ yep.java
@@ -3,8 +3,8 @@ public class SubtractTwoNumbers {
     public static void main(String[] args) {
          
         
-        int x=50;
-        int y=10;
+        double x=50;
+        double y=10;
  
         // declare another variable to hold the substracted value
         int subtract;
